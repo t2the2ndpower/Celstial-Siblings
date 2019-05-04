afa0e3fd1857cb65a0eb933aa98af5b1790b09f0
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-05-04 16:27:45 -0400

    routes working

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,11 +5,11 @@ var path = require("path");
 var htmlrouter = express.Router(); 
 
 htmlrouter.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "..public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
 htmlrouter.get("/survey", function(req, res){
-    res.sendFile(path.join(__dirname, "..public/survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
 
