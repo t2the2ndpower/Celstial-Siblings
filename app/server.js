b8126c3d8c31164e12cdba1799a9d4464164357d
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-05-04 15:26:40 -0400

    server.js updates

--- app/server.js
+++ app/server.js
@@ -1,5 +1,20 @@
 // server files
 
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var apiRoutes = require("");
+var htmlRouts = require("");
+
+app.use();
+app.use();
+app.use();
+app.use();
+
+
+app.listen();
 
