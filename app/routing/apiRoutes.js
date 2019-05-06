09494c9c9d1adfd0c3c150c9879f253dd2b46683
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-05-06 17:56:06 -0400

    working logic in the apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,25 +2,60 @@
 
 
 var express = require("express");
-var apirouter = express.Router(); 
+var apirouter = express.Router();
 var bodyParser = require("body-parser");
 var siblings = require("../data/siblings.js");
 
-apirouter.get("/api/siblings", function(req,res){
+apirouter.get("/api/siblings", function (req, res) {
     res.json(siblings);
 });
 
 
-apirouter.post("/data/newSib", function(req,res){
+apirouter.post("/data/newSib", function (req, res) {
 
     console.log(req.body);
 
     var newSibling = req.body;
 
+    var lowestScore = 41;
+
+    var sibIndex = 0;
+
+    console.log(newSibling['scores[]']);
+
+    // iterate through the the objects
+
+    for (var i = 0; i < siblings.length; i++) {
+
+        var sibScore = 0;
+
+        //iterate through the scores
+
+        for (var x = 0; x < siblings[i].scores.length; x++) {
+
+            sibScore = Math.abs(newSibling['scores[]'][x] - siblings[i].scores[x]) + sibScore;
+
+        }
+
+        //compare the scores and return the lowest result
+        
+        if (sibScore < lowestScore) {
+
+            lowestScore = sibScore;
+
+            sibIndex = i;
+
+        };
+
+    };
+
+    console.log(siblings[sibIndex]);
+
+
     siblings.push(newSibling);
 
+    res.json(siblings[sibIndex]);
 
-    res.json(siblings);
 });
 
 
